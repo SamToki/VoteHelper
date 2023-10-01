f6b156f824d7b13c4fc646dde6dd64cdc617ad3f
Sam Toki <SamToki@outlook.com>
2023-10-01 16:52:38 +0800

    v1.05 Additional update.

--- PROJECT/script.js
+++ PROJECT/script.js
@@ -90,12 +90,12 @@
             ChangeChecked("Checkbox_SettingsDisplayShowTopbar", System.Display.ShowTopbar);
             if(System.Display.ShowTopbar == true) {
                 ChangeShow("Topbar");
-                ChangeShow("SectionTitle_Primary");
-                ChangeHeight("Viewport_Primary", "");
+                ChangeShow("SectionTitleBelowTopbar");
+                ChangeHeight("ViewportBelowTopbar", "");
             } else {
                 ChangeHide("Topbar");
-                ChangeHide("SectionTitle_Primary");
-                ChangeHeight("Viewport_Primary", "100%");
+                ChangeHide("SectionTitleBelowTopbar");
+                ChangeHeight("ViewportBelowTopbar", "100%");
             }
             ChangeValue("Combobox_SettingsDisplayAnimSpd", System.Display.Anim.Spd);
             ChangeAnimSpd(System.Display.Anim.Spd);
