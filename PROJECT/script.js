d3f2eb744e56dbcd3506842fb6cde66f0b4ded40
Sam Toki <SamToki@outlook.com>
2023-11-07 12:51:42 +0800

    Following main page v1.45 update.

--- PROJECT/script.js
+++ PROJECT/script.js
@@ -33,22 +33,19 @@
 				PopupDialogAppear("System_LanguageUnsupported",
 					"Termination",
 					"<span lang='en-US'>Sorry, this page currently does not support English (US).</span>",
-					"<span lang='en-US'>OK</span>", "", "");
-				Focus("Cmdbtn_PopupDialogOption1");
+					"", "", "<span lang='en-US'>OK</span>");
 				break;
 			case "ja-JP":
 				PopupDialogAppear("System_LanguageUnsupported",
 					"Termination",
 					"<span lang='ja-JP'>すみません。このページは日本語にまだサポートしていません。</span>",
-					"<span lang='ja-JP'>OK</span>", "", "");
-				Focus("Cmdbtn_PopupDialogOption1");
+					"", "", "<span lang='ja-JP'>OK</span>");
 				break;
 			case "zh-TW":
 				PopupDialogAppear("System_LanguageUnsupported",
 					"Termination",
 					"<span lang='zh-TW'>抱歉，本頁面暫不支援繁體中文。</span>",
-					"<span lang='zh-TW'>確定</span>", "", "");
-				Focus("Cmdbtn_PopupDialogOption1");
+					"", "", "<span lang='zh-TW'>確定</span>");
 				break;
 			default:
 				alert("Error: The value of System.I18n.Language in function window.onload is out of expectation.");
@@ -307,22 +304,19 @@
 					PopupDialogAppear("System_LanguageUnsupported",
 						"Termination",
 						"<span lang='en-US'>Sorry, this page currently does not support English (US).</span>",
-						"<span lang='en-US'>OK</span>", "", "");
-					Focus("Cmdbtn_PopupDialogOption1");
+						"", "", "<span lang='en-US'>OK</span>");
 					break;
 				case "ja-JP":
 					PopupDialogAppear("System_LanguageUnsupported",
 						"Termination",
 						"<span lang='ja-JP'>すみません。このページは日本語にまだサポートしていません。</span>",
-						"<span lang='ja-JP'>OK</span>", "", "");
-					Focus("Cmdbtn_PopupDialogOption1");
+						"", "", "<span lang='ja-JP'>OK</span>");
 					break;
 				case "zh-TW":
 					PopupDialogAppear("System_LanguageUnsupported",
 						"Termination",
 						"<span lang='zh-TW'>抱歉，本頁面暫不支援繁體中文。</span>",
-						"<span lang='zh-TW'>確定</span>", "", "");
-					Focus("Cmdbtn_PopupDialogOption1");
+						"", "", "<span lang='zh-TW'>確定</span>");
 					break;
 				default:
 					alert("Error: The value of System.I18n.Language in function SetI18nLanguage is out of expectation.");
@@ -344,8 +338,7 @@
 					PopupDialogAppear("System_JSONStringFormatMismatch",
 						"Termination",
 						"JSON 字符串格式不匹配。请检查您粘贴的文本的来源。",
-						"确定", "", "");
-					Focus("Cmdbtn_PopupDialogOption1");
+						"", "", "确定");
 					RefreshSystem();
 				}
 			}
@@ -358,15 +351,13 @@
 			PopupDialogAppear("System_UserDataExported",
 				"Completion",
 				"已将用户数据以 JSON 字符串的形式导出至剪贴板。若要分享，请注意其中是否包含个人信息。",
-				"确定", "", "");
-			Focus("Cmdbtn_PopupDialogOption1");
+				"", "", "确定");
 		}
 		function SetUserDataClear() {
 			PopupDialogAppear("System_ConfirmClearUserData",
 				"Caution",
 				"您确认要清空用户数据？",
-				"清空", "取消", "");
-			Focus("Cmdbtn_PopupDialogOption2");
+				"", "清空", "取消");
 		}
 	
 	// Popup Dialog Answer
@@ -376,7 +367,7 @@
 			case "System_JSONStringFormatMismatch":
 			case "System_UserDataExported":
 				switch(Selector) {
-					case 1:
+					case 3:
 						break;
 					default:
 						alert("Error: The value of Selector in function PopupDialogAnswer is out of expectation.");
@@ -385,16 +376,18 @@
 				break;
 			case "System_ConfirmClearUserData":
 				switch(Selector) {
-					case 1:
+					case 2:
 						localStorage.clear();
 						window.location.reload();
 						break;
-					case 2:
+					case 3:
 						break;
 					default:
 						alert("Error: The value of Selector in function PopupDialogAnswer is out of expectation.");
 						break;
 				}
+				break;
+			case "":
 				break;
 			default:
 				alert("Error: The value of Interaction.PopupDialogEvent in function PopupDialogAnswer is out of expectation.");
