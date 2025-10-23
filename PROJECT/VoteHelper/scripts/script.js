a04a7d42c08adf10d8b94196eb0d978cf54724a9
Sam Toki <SamToki@outlook.com>
2025-10-23 22:10:09 +0800

    Following home page v6.25 update.

--- PROJECT/VoteHelper/scripts/script.js
+++ PROJECT/VoteHelper/scripts/script.js
@@ -285,8 +285,10 @@
 			ChangeChecked("Checkbox_SettingsTryToOptimizePerformance", System.Dev.TryToOptimizePerformance);
 			if(System.Dev.TryToOptimizePerformance == true) {
 				AddClass("Html", "TryToOptimizePerformance");
+				Automation.ClockRate = 40;
 			} else {
 				RemoveClass("Html", "TryToOptimizePerformance");
+				Automation.ClockRate = 20;
 			}
 			ChangeChecked("Checkbox_SettingsShowDebugOutlines", System.Dev.ShowDebugOutlines);
 			if(System.Dev.ShowDebugOutlines == true) {
@@ -501,9 +503,7 @@
 						ShowIAmHere("Item_SettingsUserData");
 						break;
 					case 2:
-						Object.keys(Automation).forEach(function(SubobjectName) {
-							clearTimeout(Automation[SubobjectName]);
-						});
+						ForceStop();
 						break;
 					case 3:
 						break;
